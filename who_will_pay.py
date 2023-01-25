c9e5f39fde0c94aebd4ce8bb4ef9436243bc8e13
cjnsario <cjnsario.05@gmail.com>
2023-01-25 14:40:52 +0800

    minor changes to code

--- who_will_pay.py
+++ who_will_pay.py
@@ -2,6 +2,7 @@ import random
 
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
+calculate_name = len(names)-1
+random = random.randint(0, calculate_name)
 
-random = random.randint(0, len(names)-1)
 print(f"{names[random]} is going to pay for the meal today!")
