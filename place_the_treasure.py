72934c3ca877b3c34cdf5e01a0b3f85bcb7285b2
cjnsario <cjnsario.05@gmail.com>
2023-01-25 15:24:05 +0800

    made the code more interesting

--- place_the_treasure.py
+++ place_the_treasure.py
@@ -1,25 +1,16 @@
-# 🚨 Don't change the code below 👇
-row1 = ["⬜️","⬜️","⬜️"]
-row2 = ["⬜️","⬜️","⬜️"]
-row3 = ["⬜️","⬜️","⬜️"]
-map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
-# 🚨 Don't change the code above 👆
+row1 = ["⬜️","⬜️","⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️","⬜️","⬜️"]
+row4 = ["⬜️","⬜️","⬜️","⬜️","⬜️"]
+row5 = ["⬜️","⬜️","⬜️","⬜️","⬜️"]
+map = [row1, row2, row3, row4, row5]
+print(f"{row1}\n{row2}\n{row3}\n{row4}\n{row5}")
+position = input("Where do you want to put the treasure? (Max of 55. Example: 55)")
 
-#Write your code below this row 👇
 horizontal = int(position[0])
 vertical = int(position[1])
 
 selected_row = map[vertical - 1]
 selected_row[horizontal - 1] = "X"
 
-
-
-
-
-
-#Write your code above this row 👆
-
-# 🚨 Don't change the code below 👇
-print(f"{row1}\n{row2}\n{row3}")
+print(f"{row1}\n{row2}\n{row3}\n{row4}\n{row5}")
