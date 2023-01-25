618aa29f22bce99775eb80a6870e963c9361cbd9
cjnsario <cjnsario.05@gmail.com>
2023-01-25 14:29:02 +0800

    added who will pay exercise

---
+++ who_will_pay.py
@@ -0,0 +1,7 @@
+import random
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+random = random.randint(0, len(names)-1)
+print(f"{names[random]} is going to pay for the meal today!")
